5553e5e9344159b0403e41710dfdba5e2d9bef5e
SunShineSon <ggl1314ggl@163.com>
2015-10-21 10:25:40 +0800

    1、check

---
+++ src/main/java/com/demo/string/Demo.java
@@ -0,0 +1,13 @@
+package com.demo.string;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// String str = "com.foss.user.plugin.mycodeois2.MyPlugin";
+		// System.out.println(str.substring(str.lastIndexOf(".") + 1 ));
+		String strK = "plugin_code.zip";
+		String unZipFileName = strK.substring(0, strK.indexOf("."));
+		System.out.println(unZipFileName);
+	}
+
+}
--- src/main/java/com/demo/string/DemoA.java
+++ src/main/java/com/demo/string/DemoA.java
@@ -3,7 +3,9 @@ package com.demo.string;
 public class DemoA {
 
 	public static void main(String[] args) {
-		System.out.println("ok");
+		String str = "app_id";
+		str = str.replaceAll("app_id", "app_id_value");
+		System.out.println(str);
 
 	}
 
