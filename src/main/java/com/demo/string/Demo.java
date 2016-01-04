bb1942d7f5b772ae6c74569c597194c9e1dd8069
SunShineSon <ggl1314ggl@163.com>
2016-01-04 16:46:54 +0800

    1、test

--- src/main/java/com/demo/string/Demo.java
+++ src/main/java/com/demo/string/Demo.java
@@ -3,6 +3,7 @@ package com.demo.string;
 public class Demo {
 
 	public static void main(String[] args) {
+		//test
 		// String str = "com.foss.user.plugin.mycodeois2.MyPlugin";
 		// System.out.println(str.substring(str.lastIndexOf(".") + 1 ));
 		String strK = "plugin_code.zip";
