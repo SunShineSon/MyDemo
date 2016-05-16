a56dc6b12498d13e9b2abde9a9152881a35b31fe
Administrator <Administrator@Guo-PC>
2016-05-16 10:40:19 +0800

    11111111111

--- src/main/java/com/demo/random/Demo.java
+++ src/main/java/com/demo/random/Demo.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Demo {
 	public static void main(String[] args) {
-Random random = new Random();
-		
+		Random random = new Random(47);
+		System.out.println(random.nextInt(10));
+
 		Integer randomNum = random.nextInt(1000000000);
-		
+
 		String md5Num = randomNum.toString();
 	}
 }
