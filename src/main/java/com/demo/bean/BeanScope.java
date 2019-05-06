3094563f19dc88357b5494f8185d17b1939cb219
SunShineSon <ggl1314ggl@163.com>
2019-05-06 16:09:09 +0800

    spring bean初始化对象的作用域，scope=singleton单例模式，propotype每次调用创建新对象，request模式web应用中request范围，session模式web应用中session范围。

--- src/main/java/com/demo/bean/BeanScope.java
+++ src/main/java/com/demo/bean/BeanScope.java
@@ -3,7 +3,12 @@ package com.demo.bean;
 /**
  * @Author : Guanlan
  * @Time : 2019/5/6 15:46
- * @Description :
+ * @Description : spring bean初始化对象的作用域，
+ * scope=singleton单例模式，
+ * propotype每次调用创建新对象，
+ * request模式web应用中request范围，
+ * session模式web应用中session范围。
+ *
  **/
 public class BeanScope {
 
